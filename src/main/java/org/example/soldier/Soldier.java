dc5fae73c31e0d520128a612a0eeb96e55c89770
JMPolvillo <rothkildam@gmail.com>
2024-06-18 09:54:02 +0200

    modified soldier.java

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,33 @@
 package org.example.soldier;
 
 public class Soldier {
+
+    public int strength;
+    public int health;
+
+    public Soldier (int health, int strength) {
+
+        this.health = health;
+        this.strength = strength;
+    }
+      
+    public int attack() {
+        return this.strength;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void receiveDamage(int damage) {
+        this.health = health - damage;
+    }
 }
